a3bedf737591c9d1a01e491173e63549f60c674b
yezuofan <414243885@qq.com>
2019-05-22 16:33:49 +0800

    add name

--- code01.py
+++ code01.py
@@ -3,6 +3,8 @@
 """
 import pymysql
 
+print("ddsdsdfadfaf")# tom
+
 data="127.0.0.1"
 # 1 获取数据
 float_usd = float(input("请输入美元"))
@@ -12,6 +14,11 @@ rmb = float_usd * 6.708
 print(rmb)
 
 print("在ＴOM_DEV分之上")
+
+
+print("在ＴOM_DEV分之上")
+print("在ＴOM_DEV分之上")
+
 
 # 获取数据
 
